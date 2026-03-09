2ac58167c53ccb33e6b3b2ad427e78cca2bb9185
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2026-03-09 22:50:38 +0530

    update DB

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    seed: 'tsx prisma/seed.ts',
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
